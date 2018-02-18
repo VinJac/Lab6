4ac00f12a17146b3dd720770c3418ab25aafe406
Vincent Jacob <vincent.jacob@edu.ece.fr>
2018-02-18 15:52:38 +0100

    Squelette EX3

---
+++ src/ex3/part2/Main.java
@@ -0,0 +1,13 @@
+package ex3.part2;
+/*
+ * PART 2: After Cons and Nil being inner classes
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
